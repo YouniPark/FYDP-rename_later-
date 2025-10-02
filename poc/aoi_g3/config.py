4c3d402c06d6ce0604ddb4acdcb44bc9c867b133
vivien <92554675+vivienxy@users.noreply.github.com>
2025-10-02 17:43:01 -0400

    Create config.py

---
+++ poc/aoi_g3/config.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+
+@dataclass
+class AppConfig:
+    MIN_DWELL_SEC: float = 0.100
+    COOLDOWN_SEC: float = 0.300
+
+    PROTOTXT: str = "deploy.prototxt"
+    WEIGHTS: str = "res10_300x300_ssd.caffemodel"
+    CONF_THRESH: float = 0.3
+    PROTOTXT_URL: str = "https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt"
+    WEIGHTS_URL: str = "https://github.com/opencv/opencv_3rdparty/raw/dnn_samples_face_detector_20170830/res10_300x300_ssd_iter_140000.caffemodel"
+
+    PRINT_PERIOD: float = 0.100
+
+    MARKER_NAME: str = "Markers"
+    MARKER_SOURCE_ID: str = "face_aoi_markers"
+
+    G3_TS_NAME: str = "Glasses3_VideoTS"
+    G3_GAZE_NAME: str = "Glasses3_Gaze"
